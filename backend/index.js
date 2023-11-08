168398c21ef6a7549ff31263fca9ba69539d53b4
Mo Mesgin <mmesgin@Mos-M2-MacBook-Pro.local>
2023-11-08 09:42:42 -0800

    combined fe and be

---
+++ backend/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+// const redis = require('redis');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const { handleUserConnect, checkRoom } = require('./modules/api');
+
+// express init
+const app = express();
+
+// setup cors and body-parser
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const httpServer = createServer(app);
+
+// setup redis
+// const redisClient = redis.createClient(6379, '127.0.0.1');
+
+// Connect to redis server
+// (async () => {
+//   await redisClient.connect();
+// })();
+
+// redisClient.on('connect', () => {
+//   console.log('REDIS Connected!');
+
+//   const io = new Server(httpServer, {
+//     cors: {
+//       origin: 'http://localhost:4000',
+//       methods: ['GET', 'POST'],
+//     },
+//   });
+
+//   io.on('connection', (socket) => {
+//     handleUserConnect(socket, io, redisClient);
+//   });
+// });
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:4000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  handleUserConnect(socket, io);
+});
+
+// check session id
+app.post('/checkRoom', (req, res) => {
+  const { room } = req.body;
+  const session = checkRoom(room);
+  res.send(session);
+});
+
+httpServer.listen(8080, () => {
+  console.log('Server is running...');
+});
